6449f63043cccb0fb372bd69722d64974adc6c5b
Martin Sixta <martin.sixta@gmail.com>
2013-08-29 13:23:18 +0200

    Example shell task 	* python's logger does not work at the moment

---
+++ task-test-shell/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello world from bash!"
+
+# logger does not work right now
+# echo "error" | python $logger -e
+# echo "warn"  | python $logger -w
+# echo "info"  | python $logger -i
+# echo "debug" | python $logger -d
+# echo "trace" | python $logger -t
